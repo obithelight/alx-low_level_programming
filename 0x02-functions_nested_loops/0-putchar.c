af1ca375340ba4b122af612d5a9a19830908059b
Obi Kenneth Onu <kennetho.onu@gmail.com>
2023-07-18 04:35:35 -0700

    A program that prints '_putchar', followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+	
+	int i;
+
+	for(i = 0; i < 8; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
