418427062c9048439b1966fe76157df8e8386d03
Obi Kenneth Onu <kennetho.onu@gmail.com>
2023-07-24 19:26:56 -0700

    Print a string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
 /**
- * print_rev - prints a string in reverse
- * @s: function parameter
- * Return: nothing
+ * print_rev - Prints a string in reverse.
+ * @s: string to tbe reserved.
  */
-
 void print_rev(char *s)
 {
-	int i, j;
+	int len = 0, i = 0;
 
-	for (i = 0; s[i] != '\0'; i++);
+	while (s[i++])
+		len++;
 
-	for (j = i; j >= 0; j--)
-	{
-		_putchar(s[j]);
-	}
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
+
 	_putchar('\n');
 }
