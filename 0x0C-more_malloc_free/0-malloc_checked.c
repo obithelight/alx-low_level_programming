e1f2dfaf85bf16386327c990e2c253fcf0998612
Obi Kenneth Onu <kennetho.onu@gmail.com>
2023-08-10 06:34:56 -0700

    Allocates memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: integer
+ *
+ * Return: pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *pointer;
+
+	pointer = malloc(b);
+
+	if (pointer == NULL)
+	{
+		exit(98);
+	}
+
+	return (pointer);
+}
