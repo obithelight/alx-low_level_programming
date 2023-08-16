92ce3a9545c67c994c872d71b60bbec60c958d7e
Obi Kenneth Onu <kennetho.onu@gmail.com>
2023-08-16 07:40:39 -0700

    function that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
  * print_name - prints a name
@@ -10,5 +11,5 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	(f)(name);
 }
