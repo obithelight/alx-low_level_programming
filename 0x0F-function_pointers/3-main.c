1eb3c7e5afb404b012f802f98deed0b40b8d2916
Obi Kenneth Onu <kennetho.onu@gmail.com>
2023-08-17 19:56:43 -0700

    Updated

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -23,7 +23,7 @@ int main(int argc, char **argv)
 	op = argv[2];
 	num2 = atoi(argv[3]);
 
-	if (get_op_func(op) == NULL)
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		return (-1);
