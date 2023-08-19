a1442abaedbf7ce5af42b7324c2a24c0e1e1e228
Obi Kenneth Onu <kennetho.onu@gmail.com>
2023-08-18 18:40:08 -0700

    Updated 3-main.c file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,32 +10,21 @@
 
 int main(int argc, char **argv)
 {
-	int num1, num2;
-	char *op;
+	int (*myFunc)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		return (-1);
 	}
+	myFunc = get_op_func(argv[2]);
 
-	num1 = atoi(argv[1]);
-	op = argv[2];
-	num2 = atoi(argv[3]);
-
-	if (get_op_func(op) == NULL || op[1] != '\0')
+	if (!myFunc)
 	{
 		printf("Error\n");
 		return (-1);
 	}
-
-	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
-	{
-		printf("Error\n");
-		return (-1);
-	}
-
-	printf("%d\n", get_op_func(op)(num1, num2));
+	printf("%d\n", myFunc(atoi(argv[1]), atoi(argv[3])));
 
 	return (0);
 }
