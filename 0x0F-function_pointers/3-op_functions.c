9839651e0b4cb91f506a894fe91eda5a568b5b63
Obi Kenneth Onu <kennetho.onu@gmail.com>
2023-08-18 19:09:12 -0700

    Updated 3-op_functions.c file

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -48,7 +48,7 @@ int op_div(int a, int b)
 	if (b == 0)
 	{
 		printf("Error\n");
-		return (-1);
+		exit (98);
 	}
 	return (a / b);
 }
@@ -65,7 +65,7 @@ int op_mod(int a, int b)
 	if (b == 0)
 	{
 		printf("Error\n");
-		return (-1);
+		exit (98);
 	}
 	return (a % b);
 }
