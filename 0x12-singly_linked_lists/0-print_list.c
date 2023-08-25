c674be739b18ec91a557e8f40169071919f38e5c
Obi Kenneth Onu <kennetho.onu@gmail.com>
2023-08-24 18:19:16 -0700

    Prints all the elements in a list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: elements in the list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t num_of_nodes = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		num_of_nodes++;
+		h = h->next;
+	}
+
+	return (num_of_nodes);
+}
