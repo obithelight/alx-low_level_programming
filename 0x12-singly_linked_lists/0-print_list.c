ade3d03545eb76d3b91b28162a595b12db9b729e
Obi Kenneth Onu <kennetho.onu@gmail.com>
2023-08-24 19:55:15 -0700

    Updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,15 +1,15 @@
 #include "lists.h"
 
 /**
- * print_list - Prints all the elements of a list_t list.
- * @h: The list_t list.
+ * print_list - prints all the elements of a list_t list
+ * @h: elements in the list
  *
- * Return: The number of nodes in h.
+ * Return: number of nodes
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	size_t num_of_nodes = 0;
 
 	while (h)
 	{
@@ -19,9 +19,9 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
-		nodes++;
+		num_of_nodes++;
 		h = h->next;
 	}
 
-	return (nodes);
+	return (num_of_nodes);
 }
