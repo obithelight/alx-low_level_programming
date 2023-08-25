946936329744f76c9316949f9a7f17abd7f842c1
Obi Kenneth Onu <kennetho.onu@gmail.com>
2023-08-24 19:51:09 -0700

    Print the elements in a list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,16 +1,15 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * print_list - prints all the elements of a list_t list
- * @h: elements in the list
+ * print_list - Prints all the elements of a list_t list.
+ * @h: The list_t list.
  *
- * Return: number of nodes
+ * Return: The number of nodes in h.
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t num_of_nodes = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
@@ -20,9 +19,9 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
-		num_of_nodes++;
+		nodes++;
 		h = h->next;
 	}
 
-	return (num_of_nodes);
+	return (nodes);
 }
