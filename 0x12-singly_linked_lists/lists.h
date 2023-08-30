f0b2f580dbc9414e904df997f3e46cb874714d67
Obi Kenneth Onu <kennetho.onu@gmail.com>
2023-08-30 09:12:44 -0700

    Updated lists.h file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,13 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-/*
- * File: lists.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes and definitions for all functions
- *       and types written in the 0x11-singly_linked_lists directory.
- */
-
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -20,11 +13,12 @@
  *
  * Description: singly linked list node structure
  */
+
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
