1d4bc9c017baced85e08613b8dd381a6e0681722
Obi Kenneth Onu <kennetho.onu@gmail.com>
2023-09-04 19:20:17 -0700

    a function create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,12 +18,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (length = 0; text_content[length];)
+			length++;
 	}
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
+	w = write(o, text_content, length);
 
 	if (o == -1 || w == -1)
 		return (-1);
