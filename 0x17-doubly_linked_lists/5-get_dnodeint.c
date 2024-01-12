b421b7938d6420a8b804b7f895076a03e4999e74
Obi Chukwu <kennetho.onu@gmail.com>
2024-01-12 09:47:23 +0800

    function that returns the nth node of a doubly linked list

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -4,29 +4,18 @@
  * get_dnodeint_at_index - returns the nth node of a doubly linked list
  * @head: head pointer
  * @index: index of nth node
- * Return: NULL if node does not exist
+ * Return: nth node if it exists, NULL otherwise
  */
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *temp;
-	dlistint_t *newnode;
-
-	newnode = malloc(sizeof(dlistint_t));
-	if (newnode == NULL)
-		return (NULL);
-
-	newnode->prev = NULL;
-	newnode->next = NULL;
-
-	if (head == NULL)
-		newnode = head;
 
 	temp = head;
-
-	while (temp->next != NULL)
+	while (temp != NULL && index > 0)
 	{
-		newnode->next = temp;
+		temp = temp->next;
+		index--;
 	}
-	return (newnode);
+	return (temp);
 }
