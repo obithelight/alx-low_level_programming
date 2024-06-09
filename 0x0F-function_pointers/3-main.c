caa1977e871dc80ee49e5d79523e3cd5d0d60f20
Obi Chukwu <kennetho.onu@gmail.com>
2024-06-09 12:19:08 +0100

    optimized program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,7 +13,7 @@ int main(int argc, char **argv)
 
 	if (argc != 4)
 	{
-		printf("Usage: %s <num1> <operator> <num2>\n", argv[0]);
+		printf("Error\n");
 		exit(98);
 	}
 
@@ -23,13 +23,13 @@ int main(int argc, char **argv)
 	operation = get_op_func(argv[2]);
 	if (operation == NULL)
 	{
-		printf("Error: Operator not recognized\n");
+		printf("Error\n");
 		exit(99);
 	}
 
 	if ((*(argv[2]) == '/' || *(argv[2]) == '%') && b == 0)
 	{
-		printf("Error: Division by zero\n");
+		printf("Error\n");
 		exit(100);
 	}
 
