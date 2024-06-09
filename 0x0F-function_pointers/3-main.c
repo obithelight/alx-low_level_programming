0aa13907947262db126578bff011dfd8edf2ec4f
Obi Chukwu <kennetho.onu@gmail.com>
2024-06-09 12:12:02 +0100

    optimized program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,56 +8,32 @@
  */
 int main(int argc, char **argv)
 {
-        int a, b;
-        int (*operation)(int, int);
-
-        if (argc != 4)
-        {
-                printf("Usage: %s <num1> <operator> <num2>\n", argv[0]);
-                exit(98);
-        }
-
-        a = atoi(argv[1]);
-        b = atoi(argv[3]);
-
-        operation = get_op_func(argv[2]);
-        if (operation == NULL)
-        {
-                printf("Error: Operator not recognized\n");
-                exit(99);
-        }
-
-        if ((*(argv[2]) == '/' || *(argv[2]) == '%') && b == 0)
-        {
-                printf("Error\n");
-                exit(100);
-        }
-
-        printf("%d\n", operation(a, b));
-
-        return (0);
-}
-
-/***
-
-int main(int argc, char **argv)
-{
-	int (*myFunc)(int, int);
+	int a, b;
+	int (*operation)(int, int);
 
 	if (argc != 4)
 	{
-		printf("Error\n");
+		printf("Usage: %s <num1> <operator> <num2>\n", argv[0]);
 		exit(98);
 	}
-	myFunc = get_op_func(argv[2]);
 
-	if (!myFunc)
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	operation = get_op_func(argv[2]);
+	if (operation == NULL)
 	{
-		printf("Error\n");
+		printf("Error: Operator not recognized\n");
 		exit(99);
 	}
-	printf("%d\n", myFunc(atoi(argv[1]), atoi(argv[3])));
+
+	if ((*(argv[2]) == '/' || *(argv[2]) == '%') && b == 0)
+	{
+		printf("Error: Division by zero\n");
+		exit(100);
+	}
+
+	printf("%d\n", operation(a, b));
 
 	return (0);
 }
-**/
